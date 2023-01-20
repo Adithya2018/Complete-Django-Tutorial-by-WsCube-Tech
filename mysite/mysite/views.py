63cb4c29eb6e84b067fbb57163b6dc71a4ea7e7e
Adithyan S Raj <adithyansraj20@gmail.com>
2023-01-20 11:51:52 +0530

    for loops and if statements django

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "index.html")
+    data = {
+        'title': 'Home Page',
+        'bdata': 'Welcome to WsCubeTech',
+        'clist': ['PHP', 'Java', 'C', 'Django'],
+        'student_details': [
+            {'name': 'Adi', 'phone': 244355},
+            {'name': 'SDR', 'phone': 323224}
+        ],
+        'numbers': [10, 20, 30, 40, 50]
+    }
+    return render(request, "index.html", data)
 
 
 def about_us(request):
